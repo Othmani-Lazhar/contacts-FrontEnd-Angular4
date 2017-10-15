78cc0cdf2569bb26ed7b36edf7466720b6201ad4
LAZHAR2 <othmani.lazhar@gmail.com>
2017-10-15 03:06:21 +0100

    ok final

--- ContactsWeb/src/app/app.module.ts
+++ ContactsWeb/src/app/app.module.ts
@@ -2,21 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { AboutComponent } from './about/about.component';
+import { Routes, RouterModule } from '@angular/router';
+import {HttpModule} from '@angular/http';
+import {ContactsService} from "../services/contacts.services";
+
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: 'contacts', component: ContactsComponent},
+  {path: '', pathMatch: 'full', redirectTo: '/about'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(routes),HttpModule
   ],
-  providers: [],
+  providers: [ContactsService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  
 
-  
-  
-  
+
+
+
+
 }
---
+++ ContactsWeb/src/services/contacts.services.ts
@@ -0,0 +1,15 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+@Injectable()
+export class ContactsService{
+
+  constructor (public http:Http){
+
+
+                  }
+
+  getContacts(){
+    return  this.http.get('http://localhost:8080/chercherContacts?mc=m&size=5&page=0')
+      .map(resp=>resp.json());
+  }
+}
